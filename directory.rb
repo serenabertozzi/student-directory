66253c05579b271dfd0dfa57fbb1fea2db8c4ea4
serenabertozzi <serenabertozzi@protonmail.com>
2021-09-14 20:36:31 +0100

    add exercise 1 and 2

--- directory.rb
+++ directory.rb
@@ -23,8 +23,10 @@ def print_header
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each.with_index(1) do |student, index|
+    if student[:name][0] == "S"
+      puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
+    end
   end
 end
 
