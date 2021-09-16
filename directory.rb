ded6bbab089f8e420dd1bbda43a7a41ac0492dc9
serenabertozzi <serenabertozzi@protonmail.com>
2021-09-16 16:14:56 +0100

    add git ignore

--- directory.rb
+++ directory.rb
@@ -30,11 +30,11 @@ end
 
 def save_students
   # open the file for writing
-  file = File.open("studens.csv", "w")
+  file = File.open("students.csv", "w")
   # iterate over the array of students
   @students.each do |student|
     student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(",")
+    csv_line = student_data.join(", ")
     file.puts csv_line
   end
   file.close
