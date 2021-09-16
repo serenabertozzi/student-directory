4daf9b50059c786248840c733c7576c5bdfcef77
serenabertozzi <serenabertozzi@protonmail.com>
2021-09-16 14:39:30 +0100

    refactor of interactive menu

--- directory.rb
+++ directory.rb
@@ -1,11 +1,10 @@
 require "date"
 Date::MONTHNAMES[1..]
+@students = []
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  #create an empty array
-  students = []
   # get the first name
   name = gets.gsub("\n", "")
   #while the name is not empty, repeat this code
@@ -19,17 +18,44 @@ def input_students
       end
       if Date::MONTHNAMES[1..].include?(cohort) || cohort = :unknown
         #add the students hash to the array
-        students << { name: name, cohort: cohort.to_sym, hobby: "causing troubles", country_of_birth: :nowhere, height: :unknown }
-        puts "Now we have #{students.count} #{students.count > 1 ? "students" : "student"}"
+        @students << { name: name, cohort: cohort.to_sym, hobby: "causing troubles", country_of_birth: :nowhere, height: :unknown }
+        puts "Now we have #{@students.count} #{@students.count > 1 ? "students" : "student"}"
         break
       else puts "This doesn't seem quite right..."       end
     end
     #get another name from the user
     name = gets.gsub("\n", "")
   end
-  #return the array of students
-  if !students.empty?
-    students
+end
+
+def intreractive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit "
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else puts "I don't know what you meant, try again"
   end
 end
 
@@ -38,9 +64,9 @@ def print_header
   puts "------------------------"
 end
 
-def print(students)
+def print_students_list
   cohorts = {}
-  students.each do |student|
+  @students.each do |student|
     if cohorts[student[:cohort]].nil?
       cohorts[student[:cohort]] = []
     end
@@ -51,32 +77,8 @@ def print(students)
   end
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} great #{students.count > 1 ? "students" : "student"}"
-end
-
-def intreractive_menu
-  students = []
-  loop do
-    #1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit "
-    #2. read the input and save it into a variable
-    selection = gets.chomp
-    #3. do what the user has asked
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit #this will cause the program to terminate
-    else puts "I don't know what you meant, try again"
-    end
-  end
+def print_footer
+  puts "Overall, we have #{@students.count} great #{@students.count > 1 ? "students" : "student"}"
 end
 
 intreractive_menu
